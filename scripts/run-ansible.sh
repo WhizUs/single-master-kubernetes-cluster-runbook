bc7bddf80d3ec04e6904165b25941ade6f8a5197
erikauer <erikauer@gmx.at>
2018-09-25 21:25:44 +0200

    uncomment anible-playbooks in run-ansible again

--- scripts/run-ansible.sh
+++ scripts/run-ansible.sh
@@ -1,3 +1,3 @@
-#ansible-playbook ./ansible-playbook/etcd-provisioning.yml -i ./inventory
-#ansible-playbook ./ansible-playbook/kubernetes-master-provisioning.yml -i ./inventory -v
+ansible-playbook ./ansible-playbook/etcd-provisioning.yml -i ./inventory
+ansible-playbook ./ansible-playbook/kubernetes-master-provisioning.yml -i ./inventory
 ansible-playbook ./ansible-playbook/kubernetes-worker-provisioning.yml -i ./inventory
