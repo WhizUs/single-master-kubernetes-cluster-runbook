1d91e49ab00898039529f1803f186e5bf4ae24b3
erikauer <erikauer@gmx.at>
2018-10-14 10:59:19 +0200

    fix run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -3,4 +3,4 @@ terraform validate -var-file='./exoscale.tfvars'
 terraform plan -var-file='./exoscale.tfvars' -out=next-steps.plan
 terraform apply -parallelism=10 next-steps.plan
 sleep 5
-sh ./scripts/run-all-ansible.sh
+sh ./scripts/run-ansible.sh
